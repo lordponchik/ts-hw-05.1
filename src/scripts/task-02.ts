055487cefd9a962628f014d5457584fd376eb37a
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-08-14 12:27:44 +0200

    add task-02

--- src/scripts/task-02.ts
+++ src/scripts/task-02.ts
@@ -1,4 +1,15 @@
-console.log('task-01');
+import { Users } from '../../users';
+import { users } from '../../users';
+
+console.log('task-02');
 console.log('');
+
+type GetUsersWithEyeColor = (users: Users, color: string) => Users;
+
+const getUsersWithEyeColor: GetUsersWithEyeColor = (users, color) => {
+  return users.filter(({ eyeColor }) => eyeColor === color);
+};
+
+console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
 export {};

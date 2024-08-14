61b9eef7407580062a625675ae40defde2af0dd7
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-08-14 19:05:39 +0200

    add task-03

--- src/scripts/task-03.ts
+++ src/scripts/task-03.ts
@@ -1,4 +1,15 @@
-console.log('task-01');
+import { Users } from '../../users';
+import { users } from '../../users';
+
+console.log('task-03');
 console.log('');
+
+type GetUsersWithGender = (users: Users, gender: string) => string[];
+
+const getUsersWithGender: GetUsersWithGender = (users, gender) => {
+  return users.filter(user => user.gender === gender).map(({ name }) => name);
+};
+
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
 export {};

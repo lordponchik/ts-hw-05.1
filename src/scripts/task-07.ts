748e075f263fcfafd234729b5e02d8bc53444af3
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-08-15 08:45:59 +0200

    add task-07

--- src/scripts/task-07.ts
+++ src/scripts/task-07.ts
@@ -1,4 +1,15 @@
-console.log('task-01');
+import { Users } from '../../users';
+import { users } from '../../users';
+
+console.log('task-07');
 console.log('');
+
+const calculateTotalBalance = (users: Users): number => {
+  return users.reduce((sum, { balance }) => {
+    return (sum += balance);
+  }, 0);
+};
+
+console.log(calculateTotalBalance(users)); // 20916
 
 export {};

647accbaca372ea4847397d46e8e5512a0fca6c4
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-08-15 08:42:33 +0200

    add task-06

--- src/scripts/task-06.ts
+++ src/scripts/task-06.ts
@@ -1,4 +1,18 @@
-console.log('task-01');
+import { Users } from '../../users';
+import { users } from '../../users';
+
+console.log('task-06');
 console.log('');
+
+type GetUsersWithAge = (users: Users, min: number, max: number) => Users;
+
+const getUsersWithAge: GetUsersWithAge = (users, min, max) => {
+  return users.filter(({ age }) => age >= min && age <= max);
+};
+
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
 export {};

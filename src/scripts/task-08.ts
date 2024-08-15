339ebad4102588bb23ae99db213a068e7d7d5fbf
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-08-15 08:49:23 +0200

    add task-08

--- src/scripts/task-08.ts
+++ src/scripts/task-08.ts
@@ -1,4 +1,16 @@
-console.log('task-01');
+import { Users } from '../../users';
+import { users } from '../../users';
+
+console.log('task-08');
 console.log('');
+
+type GetUsersWithFriend = (users: Users, friendName: string) => string[];
+
+const getUsersWithFriend: GetUsersWithFriend = (users, friendName) => {
+  return users.filter(({ friends }) => friends.includes(friendName)).map(({ name }) => name);
+};
+
+console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
 export {};

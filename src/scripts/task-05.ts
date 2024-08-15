0de716ea3543e82391081cce224f1f820a1ed14d
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-08-15 08:37:09 +0200

    add task-05

--- src/scripts/task-05.ts
+++ src/scripts/task-05.ts
@@ -1,4 +1,18 @@
-console.log('task-01');
+import { Users } from '../../users';
+import { User } from '../../users';
+import { users } from '../../users';
+
+console.log('task-05');
 console.log('');
+
+type GetUserWithEmail = (users: Users, email: string) => User | string;
+
+const getUserWithEmail: GetUserWithEmail = (users, email) => {
+  return users.find(user => user.email === email) || 'Not found';
+};
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+console.log(getUserWithEmail(users, 'wasd@omatom.com')); // "not found"
 
 export {};

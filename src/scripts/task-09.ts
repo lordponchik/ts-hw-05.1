69684bf93e36424a2993856ef3d356dbd7ab9df3
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-08-15 08:53:01 +0200

    add task-09

--- src/scripts/task-09.ts
+++ src/scripts/task-09.ts
@@ -1,4 +1,16 @@
-console.log('task-01');
+import { Users } from '../../users';
+import { users } from '../../users';
+
+console.log('task-09');
 console.log('');
+
+const getNamesSortedByFriendsCount = (users: Users): string[] => {
+  return users
+    .sort((user_1, user_2) => user_1.friends.length - user_2.friends.length)
+    .map(({ name }) => name);
+};
+
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
 export {};

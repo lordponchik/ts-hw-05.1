b8daff123e945836862ee20ea07ae911b50fa592
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-08-15 08:26:33 +0200

    add task-04

--- src/scripts/task-04.ts
+++ src/scripts/task-04.ts
@@ -1,4 +1,15 @@
-console.log('task-01');
+import { Users } from '../../users';
+import { users } from '../../users';
+
+console.log('task-04');
 console.log('');
+
+type GetInactiveUsers = (users: Users) => Users;
+
+const getInactiveUsers: GetInactiveUsers = users => {
+  return users.filter(({ isActive }) => !isActive);
+};
+
+console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
 export {};

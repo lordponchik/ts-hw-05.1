83352c12e125325021f6407eeeec8f36a089d0ea
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-08-15 08:58:48 +0200

    add task-10

--- src/scripts/task-10.ts
+++ src/scripts/task-10.ts
@@ -1,4 +1,17 @@
-console.log('task-01');
+import { Users } from '../../users';
+import { users } from '../../users';
+
+console.log('task-10');
 console.log('');
+
+const getSortedUniqueSkills = (users: Users): string[] => {
+  return users
+    .flatMap(({ skills }) => skills)
+    .filter((skill, index, arr) => arr.indexOf(skill) === index)
+    .sort((skill_1, skill_2) => skill_1.localeCompare(skill_2));
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
 
 export {};
